1920391822db939509d52e43d1b461996760eed0
Gilson <gilsonmirandajunior2@gmail.com>
2021-05-05 13:27:09 -0300

    modicação laços

--- lista1/sort.java
+++ lista1/sort.java
@@ -1,13 +1,26 @@
+/* Exercício adicional 2
+* Faça um programa que entre com três números e coloque em ordem crescente.
+*/
+
 package exercicioJava;
 
-import java.util.Arrays; 
+import java.util.Arrays; // classe para ordenar arrays
+import java.util.Scanner;
 
-public class sort { 
-	public static void main(String[] args) 
-	    { 
-	        int[] intArray = {40, 2, 20}; 	 
-	        Arrays.sort(intArray); 
-	 
-	        System.out.printf("\n\nSorted Array : %s", Arrays.toString(intArray)); 
-	    } 
+public class sort {
+   public static void main(String[] args) {
+       
+      int[] array = new int[3]; //criação de array
+      
+      for (int counter = 0; counter < array.length; counter++) { 
+          Scanner values = new Scanner(System.in);    
+           System.out.println("Digite um valor: ");
+          
+          array[counter] = values.nextInt(); //introdução de valor no array de acordo com o contador (0 a 3)
+          
+      }
+      
+      Arrays.sort(array);//ordenar crescentemente os valores do array
+      System.out.println("Os números ordandos são: " + Arrays.toString(array)); //exibir os valores do array convertidos para string (visualização como array)
+   }
 }
